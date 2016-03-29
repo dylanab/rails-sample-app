58dd14ac999a8e4203a0501e8cb89c18d4fae1a1
Dylan Bowne <dylanab@uga.edu>
2016-03-29 00:35:20 +0000

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
     
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
     
-    before_save {self.email = self.email.downcase}
+    before_save :downcase_email
+    before_create :create_activation_digest
     validates(:name, presence: true);
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates(:email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false});
@@ -29,10 +30,11 @@ class User < ActiveRecord::Base
         update_attribute(:remember_digest, User.digest(remember_token))
     end
     
-    #returns true if the given token matches the user's digest
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    #returns true if the given token matches the digest
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
     
     #forgets a user
@@ -40,4 +42,28 @@ class User < ActiveRecord::Base
        update_attribute(:remember_digest, nil) 
     end
     
+    #activated a user account
+    def activate 
+        update_attribute(:activated, true)
+        update_attribute(:activated_at, Time.zone.now)
+    end
+    
+    #sends an activation email
+    def send_activation_email
+       UserMailer.account_activation(self).deliver_now 
+    end
+    
+    
+    private 
+    
+        #converts an email to all lowercase
+        def downcase_email
+            self.email = self.email.downcase 
+        end
+    
+        def create_activation_digest 
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(self.activation_token)
+        end
+        
 end
